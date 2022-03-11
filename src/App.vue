ed6c91991f71838b24085bdc30d4cbd48caefeca
Jeffrey <jeffreyhuang0331@gmail.com>
2022-03-12 07:41:14 +0800

    feat: add event listeners

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,27 @@
 <script setup>
+import { ref, onBeforeUnmount } from 'vue'
+
+// https://developer.mozilla.org/en-US/docs/Web/API/Window/resize_event
+const width = ref(0)
+const height = ref(0)
+
+function reportWindowSize() {
+  width.value = window.innerWidth
+  height.value = window.innerHeight
+}
+
+reportWindowSize()
+
+window.addEventListener('resize', reportWindowSize)
+
+onBeforeUnmount(() => {
+  window.removeEventListener('resize', reportWindowSize)
+})
 </script>
 
 <template>
-  <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+  <main>
+    <p>Window Width: {{ width }}</p>
+    <p>Window Height: {{ height }}</p>
+  </main>
 </template>
