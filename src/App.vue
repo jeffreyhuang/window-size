b4e39c357050d4095f5fbc53706f9103a5134669
Jeffrey Huang <jeffreyhuang@users.noreply.github.com>
2022-03-16 18:31:34 +0800

    Revert "Release/0.2.0"

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,34 @@
 <script setup>
-import SizeSet from '@/components/SizeSet.vue'
-import { useViewportSize } from '@/composables/use-viewport-size'
+import SizeDisplay from '@/components/SizeDisplay.vue'
+import { useWindowSize } from '@/composables/use-window-size'
 
-const { viewportW, viewportH, windowW, windowH, screenW, screenH } = useViewportSize()
+const { width, height } = useWindowSize()
 </script>
 
 <template>
   <div class="h-screen flex justify-center items-center bg-slate-200">
     <main>
-      <div class="mb-5">
-        <SizeSet
-          :width="viewportW"
-          :height="viewportH"
-          title="Viewport Size"
+      <div class="flex justify-center items-end flex-wrap">
+        <SizeDisplay
+          :size="width"
+          title="Width"
+        />
+
+        <div class="mx-2 xl:mx-3 2xl:mx-4">
+          <span class="text-slate-800 font-semibold text-lg xl:text-2xl">
+            &times;
+          </span>
+        </div>
+
+        <SizeDisplay
+          :size="height"
+          title="Height"
         />
       </div>
 
-      <div class="mb-5">
-        <SizeSet
-          :width="windowW"
-          :height="windowH"
-          title="Window Size"
-        />
-      </div>
-
-      <div>
-        <SizeSet
-          :width="screenW"
-          :height="screenH"
-          title="Screen Resolution"
-        />
-      </div>
+      <p class="text-slate-500 text-center mt-1.5 text-xs xl:text-sm">
+        is your current window size
+      </p>
     </main>
   </div>
 </template>
